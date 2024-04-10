b1303642024be2d78daa361b1827f00b6709f1c1
JZ <jitendra.zaa@gmail.com>
2024-04-10 09:44:14 -0500

    Backup 2

--- AWS/lambda.py
+++ AWS/lambda.py
@@ -3,51 +3,29 @@ import json, os, ai21, boto3
 def lambda_handler(event, context):
     # Example of incoming request via JSON object {"prompt" : "What day is it?"}
     # parse the prompt from the incoming event object
-    prompt = 'What day is it?'
 
+    prompt_text = f"""Human: Can you tell me a short story
+    Assistant:"""
+
+    body = {
+        "prompt":prompt_text,
+        "max_tokens_to_sample":5000,
+        "temperature":0.3,
+        "top_k":250,
+        "top_p":0.2,
+        "stop_sequences": ["\n\nHuman:"]
+    }
+
+    # calling the bedrock API
+    bedrock = boto3.client("bedrock-runtime", region_name="us-east-1",
+                           config = botocore.config.Config(read_timeout=300, retries = {'max_attempts':3}))
     
-    # sets the AI21 Labs API key
-    ai21.api_key = '3lnD86XXWECZPjNgTvOmpDtqLuupM5NF'
-
-    client = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
-
-    wrapper = BedrockRuntimeWrapper(client)
-
+    response = bedrock.invoke_model(body=json.dumps(body),modelId="j2-ultra")
+    response_content = response.get('body').read().decode('utf-8')
+    response_data = json.loads(response_content)
+    summary = response_data["completion"].strip()
     
-    # returns the models result in the body of the JSON object
-    return wrapper.invoke_jurassic2(prompt)
+    return summary 
     
-def invoke_jurassic2(self, prompt):
-    """
-    Invokes the AI21 Labs Jurassic-2 large-language model to run an inference
-    using the input provided in the request body.
 
-    :param prompt: The prompt that you want Jurassic-2 to complete.
-    :return: Inference response from the model.
-    """
-
-    try:
-        # The different model providers have individual request and response formats.
-        # For the format, ranges, and default values for AI21 Labs Jurassic-2, refer to:
-        # https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-jurassic2.html
-
-        body = {
-            "prompt": prompt,
-            "temperature": 0.5,
-            "maxTokens": 200,
-        }
-
-        response = self.bedrock_runtime_client.invoke_model(
-            modelId="ai21.j2-mid-v1", body=json.dumps(body)
-        )
-
-        response_body = json.loads(response["body"].read())
-        completion = response_body["completions"][0]["data"]["text"]
-
-        return completion
-
-    except ClientError:
-        logger.error("Couldn't invoke Jurassic-2")
-        raise
-
-
+ 
